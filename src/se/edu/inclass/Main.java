541dbad4da21a726be29ae78e5d7f1a81dfec788
Zhang Yilin <e0377000@u.nus.edu>
2020-09-18 17:01:41 +0800

    print data using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -22,6 +22,9 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDeadlineUsingStreams(tasksData);
+
+        System.out.println("Total number of deadlines using streams: " + countDeadlineUsingStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
